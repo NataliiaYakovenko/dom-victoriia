63fba73df6f4140748f8cb3db557937ca34b196b
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-19 17:22:18 +0200

    Add DOM практика

--- index.js
+++ index.js
@@ -1,58 +1,41 @@
-//Атрибути
-//Атрибути елемента - це властивості відповідних об'єктів
+/*
+Задача. При натисканні на кнопку
 
-//Через властивості
-const p1 = document.querySelector("#p1");
-console.log(p1);
-console.dir(p1);
-console.log(p1.title);
-console.log(p1.id);
+Отримати заголовок першого рівня
+- встановити для нього інший кольор тла
+- виставити для заголовків другого рівня шрифт 20рх і їх колір
 
-//змінюємо атрибуе title
-p1.title = "New title";
+Встановити src="" alt="" і розміри для головного зображення
+Встановити src="" alt="" і розміри для зображення в головному артиклі
+*/
 
-//Атрибут boolean задання
-//p1.hidden = true;
+/*Алгоритм
+1. Отримати посилання на кнопку
+2. Задати функцію обробник і навісити її як обробник події на клік
+3. Наповнити цю функцію певним змістом
+   - отримати посилання на h1
+   - встановити для h1 кольор тла
 
-//Атрибут style
-p1.style.color = "green";
-p1.style.backgroundColor = "pink";
-//----------------------------------
+*/
 
-//Через методи
-const p2 = document.querySelector("#p2");
-console.log(p2);
-console.dir(p2);
+const btn = document.querySelector('button')
 
-//встановлюємо значення атрибуту
-p2.setAttribute("title", "Title of p2"); //p id="p2" title ='Title of 2'
+function getClickHandler(event){
+ const h1 = document.querySelector('h1')
+ h1.style.backgroundColor = 'blue'
 
-p2.setAttribute("style", "font-size: 30px; color: orange;"); // style = 'font - size: 30px; color: 'orange''
+ const img = document.querySelector('.mainImg')
+ img.src="https://vetmarlborough.co.nz/wp-content/uploads/cat-facts.jpg";
+ img. alt="cat";
+ img.style.width = '300px';
+ img.style.hight = '300px';
+ img.style.objectFit = 'cover';
 
-//якщо boolean
-//p2.setAttribute('hidden','hidden') //в качестві значення вказуться таке саме як іключ
+ const h2 = document.querySelectorAll('h2')
+ h2. forEach((h2)=>{
+     h2.style.fontSize = '20px';
+     h2.style.color = 'green';
+ })
 
-console.log(p2.getAttribute("style"));
-
-//задати для body будь-який колір
-//document.querySelector('body').style.backgroundColor='grey'
-const body = document.querySelector("body");
-body.style.backgroundColor = "grey";
-
-//------------------------------------------------------------------
-//Атрибут, який дозволяє змінити вміст якогось елемента
-//Наприклад у р2 потрібно змінити текст
-p2.textContent =
-  "New content dkaldjaljdalda;lda;ldkal;dkaldka;lkdasl;dkal;dka;lk";
-//--------------------------------------------------------------------
-
-//Задати атрибути зображення по об'єктам
-const imgAttr = {
-  src: "https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg",
-  alt: "cat",
-};
-const imgEl = document.querySelector('img')
-imgEl.src = imgAttr.src;
-imgEl.alt = imgAttr.alt;
-//Задати розміри зображення
-imgEl.setAttribute('width','400')
+}
+btn.addEventListener('click',getClickHandler)
