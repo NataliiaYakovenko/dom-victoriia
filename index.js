69a58aa5980c3f674835e6ef871780417705f88c
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-19 14:07:49 +0200

    Add DOM обробка подій

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
-console.log(document);
+//Обробка подій
+/*Обробник подій - це певна функція, яка здійсниться наприклад при натисканні кнопки
+Щоб навісити обробник на певну подію, нам потрібно мати доступ до елемента, тобто отримати посилання на елемент
+через будь - який метод getElementById/getElementsByClassName/ getElementsByClassName
+У властивість елемента onclick ми кладемо(присвоюємо функцію), це саме та функція, яка буде виконана, коли настанить подія
 
-console.log(document.head);
+*/
+const btn = document.getElementById('clickBtn');
+console.log(btn);
+console.dir(btn);
 
-console.log(document.body);
+const clickHander = function (event){ // прописується подія
+console.log('Btn was clicked');
+};
+btn.onclick = clickHander;//запустити кнопку
 
-const rootEl = document.getElementById('root');
-console.log(rootEl);
-console.dir(rootEl);
+btn.onclick = null // зняти обробник, зробити кнопку неактивною
+//--------------------------------------------------------------
+/*У об'єкта loginBtn викликаємо метод addEventListener 
+і у якості параметрів передати (подія, обробник)
+Наша задача написати обробник
 
-const divEls = document.getElementsByTagName('div')
-console.log(divEls);
+*/
+const loginBtn = document.getElementById('loginBtn');
+console.log(loginBtn)
 
-const divRoot2 = divEls[0]
-console.log(divRoot2);
+function loginBtnHandler(event){
+   alert('Login in process');
+}
+//addEventListener - навішувати обробник
+loginBtn.addEventListener('click',loginBtnHandler); //'click' - подія,loginBtnHandler -посилальник  
 
-const contantDivs = document.getElementsByClassName('content')
-console.log(contantDivs)
+//removeEventListener - знімати обробник
+loginBtn.removeEventListener('click',loginBtnHandler);  //зняти подію
+//----------------------------------------------------------------
 
-const rootEl2 = document.querySelector('#root')
-console.log(rootEl2);
+//Виводити "Click me!" коли наводимо курсивом миші на конпу loginBtn
+function loginMouseMoveHandler(event){
+console.log("Click me!");
+}
 
-const divsAll = document.querySelectorAll('div')
-console.log(divsAll)
+loginBtn.addEventListener('mousemove', loginMouseMoveHandler)
+//----------------------------------------------------------------
 
-const contentAll = document.querySelectorAll('.content')
-console.log(contentAll);
+//Створити конпу і при натисканні на неї виводити "Hello world!"
+
+const click = document.getElementById('clickWorld')
+console.log(click);
+
+function clickWorld(event){
+ alert('Hello world!')
+}
+
+click.onclick = clickWorld;
