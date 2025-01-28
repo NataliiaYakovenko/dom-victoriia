0c7c852c5a089d67dd465bb762b3500caf4e526c
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-28 23:01:27 +0200

    Add Занурення і всплиття

--- index.js
+++ index.js
@@ -1,46 +1,52 @@
-/*Алгоритм 
-1. Отримати посилання на кнопки
-2. Створити змінну для counter(лічильник)
-3. Навісити обробник на кнопку(-)
-4. Навісити обробник на кнопку(+)
-5. В обробнику зменшити counter(лічильник)
+//Керування розповсюдження подій
+
+const body = document.body;
+const section = document.querySelector("section");
+const button = document.querySelector("button");
+
+/*button.addEventListener('click', buttonClickHandler)
+function buttonClickHandler(event){
+console.log(event);
+console.log(this)
+console.log(event.target);
+console.log(event.currentTarget);
+
+//якщо я хочу змінити колір кнопки
+this.style.backgroundColor = 'blue'
+}
+
+function clickHandler(event){
+  console.log(event);
+  console.log(this)
+  console.log(event.target);
+  console.log(event.currentTarget);
+  console.log('-----------------------------------------');
+ 
+  event.target.style.backgroundColor = 'red'
+  }
+  
+  //Занурення і всплиття - порядок виклику обробників
+
+  //На занурення
+  body.addEventListener('click', clickHandler,{capture:true})
+  section.addEventListener('click', clickHandler,{capture:true})
+  button.addEventListener('click', clickHandler,{capture:true})
+
+  //На всплиття
+  button.addEventListener('click', clickHandler)
+  section.addEventListener('click', clickHandler)
+  body.addEventListener('click', clickHandler)
 */
+//Припинити розповсюдження події
 
-//Model - данні + методи роботи з ним
-class Counter {
-  constructor(counter) {
-    this.counter = counter;
-  }
+let clickCounter = 0;
 
-  decrement() {
-    this.counter--;
-  }
+body.addEventListener("click", () => {
+ clickCounter++ ;
+ console.log(clickCounter)
+});
 
-  increment() {
-    this.counter++;
-  }
-}
+button.addEventListener("click", (event) => {
+  event.stopPropagation(); //припинення розповсюдження події
 
-//ViewModel
-const [decBtn, incBtn] = document.querySelectorAll(".counterBtn");
-const counterElement = document.querySelector("#counter");
-
-const count = new Counter(0);
-
-updateView();
-
-decBtn.addEventListener("click", decrementCount);
-function decrementCount(event) {
-  count.decrement();
-  updateView();
-}
-
-incBtn.addEventListener("click", incrementCount);
-function incrementCount(event) {
-  count.increment();
-  updateView();
-}
-
-function updateView() {
-  counterElement.textContent = count.counter;
-}
+});
