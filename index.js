923bdc39209c8b44a62bc2f43880963fab73019e
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-01 10:07:06 +0200

    Add Рефакторінг

--- index.js
+++ index.js
@@ -22,45 +22,51 @@ const user = {
 Обовєязкові
 6. Вбудуваи його туди куди потрібно
 */
-const userCardEl = document.createElement('article');
-userCardEl.classList.add('user-card')
+const userCardEl = document.createElement("article");
+userCardEl.classList.add("user-card");
 document.body.append(userCardEl);
 
-const userImg = document.createElement('img');
-userImg.src = user.photoSrc;
-userImg.alt = `${user.firstName} ${user.lastName}`;
-userImg.classList.add('user-image');
-userCardEl.append(userImg);
+const userImg = creatImg(user);
+const userInfo = creatUserInfo(user);
+const trash = creatTrash();
 
-const userInfo = document.createElement('div');
-userInfo.classList.add('user-info');
-userCardEl.append(userInfo);
+userCardEl.append(userImg,userInfo,trash);
 
-const userNameEl = document.createElement('p');
-userNameEl.classList.add('user-name');
-userNameEl.textContent = `${user.firstName} ${user.lastName}`;
-userInfo.append(userNameEl);
 
-const userAge = document.createElement('span');
-userAge.classList.add('user-age');
-userAge.textContent = user.age;
-userInfo.append(userAge);
-
-const trash = document.createElement('i');
-trash.classList.add('fa-solid');
-trash.classList.add('fa-trash');
-trash.classList.add('trash-icon');
-
-function deleteTfash(event){
-  //userCardEl.remove();
-  //event.target.parentElement.remove();
-  event.target.closest('.user-card').remove();
+function creatTrash(){
+  const trash = document.createElement("i");
+  trash.classList.add("fa-solid");
+  trash.classList.add("fa-trash");
+  trash.classList.add("trash-icon");
+  
+  function deleteTfash(event) {
+    event.target.closest(".user-card").remove();
+  }
+  trash.onclick = deleteTfash;
+  return trash;
 }
-trash.onclick = deleteTfash;
 
-userCardEl.append(trash);
+function creatUserInfo({firstName,lastName,age}) {
+  const userInfo = document.createElement("div");
+  userInfo.classList.add("user-info");
+  
+  const userNameEl = document.createElement("p");
+  userNameEl.classList.add("user-name");
+  userNameEl.textContent = `${firstName} ${lastName}`;
+  userInfo.append(userNameEl);
+  
+  const userAge = document.createElement("span");
+  userAge.classList.add("user-age");
+  userAge.textContent = age;
+  userInfo.append(userAge);
+  
+  return userInfo;
+}
 
-
-
-
-
+function creatImg({ photoSrc, firstName, lastName }) {
+  const userImg = document.createElement("img");
+  userImg.src = photoSrc;
+  userImg.alt = `${firstName} ${lastName}`;
+  userImg.classList.add("user-image");
+  return userImg;
+}
