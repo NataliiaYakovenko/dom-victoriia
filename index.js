f70e340859593a91dff06f312972313705c7920d
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-01 08:19:10 +0200

    Add Генерація userCard

--- index.js
+++ index.js
@@ -1,32 +1,64 @@
-'use strict'
+"use strict";
 
-const section = document.createElement('section')
-const divRoot = document.querySelector('#root')
+const user = {
+  firstName: "Roland",
+  lastName: "Simonyan",
+  age: 27,
+  photoSrc:
+    "https://img.freepik.com/free-psd/3d-illustration-person-with-long-hair_23-2149436197.jpg?ga=GA1.1.1872930788.1728835828&semt=ais_hybrid",
+};
+/*
+Алгоритм
 
-//додавання дочірнього елемента 
-//append -  додає елемент в кінець колекції (масиву, списку, рядка і т. п.).
-//preppend -  додає елемент на початок колекції.
+Обовєязкові
+1. Створити елемент
 
-divRoot.append(section);
-section.textContent = 'Lorem ipsum';
-section.style.backgroundColor = 'grey'
+Необхідні
+2. Задати значення атрибута
+3. Якщо необхідно.Додати класи
+4. Додати контент
+5. Додати обробник на якусь подію
 
-const article = document.createElement('article');
-divRoot.prepend(article);
-article.textContent = 'Lorem ipsum';
-article.style.backgroundColor = 'yellow'
-//--------------------------------------------
+Обовєязкові
+6. Вбудуваи його туди куди потрібно
+*/
+const userCardEl = document.createElement('article');
+userCardEl.classList.add('user-card')
+document.body.append(userCardEl);
 
-//додаванн сіблінгів 
-//before
-//after
+const userImg = document.createElement('img');
+userImg.src = user.photoSrc;
+userImg.alt = `${user.firstName} ${user.lastName}`;
+userImg.classList.add('user-image');
+userCardEl.append(userImg);
 
-const button = document.createElement('button');
-section.after(button)
-button.textContent = 'button';
+const userInfo = document.createElement('div');
+userInfo.classList.add('user-info');
+userCardEl.append(userInfo);
 
-//видалення елементу
-const divRemove = document.querySelector('.remove')
-divRemove.addEventListener('click', function(){
-  this.remove();
-})
+const userNameEl = document.createElement('p');
+userNameEl.classList.add('user-name');
+userNameEl.textContent = `${user.firstName} ${user.lastName}`;
+userInfo.append(userNameEl);
+
+const userAge = document.createElement('span');
+userAge.classList.add('user-age');
+userAge.textContent = user.age;
+userInfo.append(userAge);
+
+const trash = document.createElement('i');
+trash.classList.add('fa-solid');
+trash.classList.add('fa-trash');
+trash.classList.add('trash-icon');
+
+function deleteTfash(event){
+  userCardEl.remove();
+}
+trash.onclick = deleteTfash;
+
+userCardEl.append(trash);
+
+
+
+
+
