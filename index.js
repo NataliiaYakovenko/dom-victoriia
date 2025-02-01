8ea9b8831b632e094efc6fb9f76ffa2c304dbe42
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-01 19:35:49 +0200

    Add setTimeout,setInterval,clearTimeout,clearInterval

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
 "use strict";
 
-//Користувацькі атрибути або ще їх називають data-*
+//setTimeout - виконати дію через певний час
 
-const btns = document.querySelectorAll("button");
+//setInterval - виконувати одну і ту саму дію з певним інтервалом
 
-//data-host => dataset.host
-function btnClickHandler(e) {
-  console.log(e.target.dataset.host);
+function timeoutAction() {
+  console.log("hello");
 }
-btns.forEach(b=>{b.onclick = btnClickHandler});
+console.log("before timeout");
+const timeoutId = setTimeout(timeoutAction, 3000);
+console.log("after timeout");
+clearTimeout(timeoutId); // приостановити дію
 
-//data-host => dataset.host
-console.log(btns[0].dataset.username);
-console.log(btns[1].dataset.userName);
-//-------------------------------------------------
-
-//В контейнер помістити 4 кнопки
-//В залежності від того на яку натискаєш,
-// зінюється колір body
-
-const buttons = document.querySelectorAll('button');
-const div = document.querySelector('div');
-
-function btnColorClickHandler(e) {
- document.body.style.backgroundColor =  e.target.dataset.color;
+let i = 0;
+function intervalHandler() {
+  if (i === 5) {
+    return clearInterval(intervalId);
+  }
+  console.log(++i);
 }
-buttons.forEach(b=>{b.onclick = btnColorClickHandler});
+
+console.log("before interval");
+const intervalId = setInterval(intervalHandler, 2000);
+console.log("after interval");
+//clearInterval(intervalId) // приостановити дію
+
+
+function deleteBtn(){
+  const btn = document.querySelector('button');
+  btn.remove();
+}
+setTimeout(deleteBtn, 3000); //автоматичне видалення кнопки через 3 секунди
+//-----------------------------------------------------
+
+//При натисканні на кнопку перефарбувати її через 2 секунди
+
+const changeBtn = document.querySelector('.btn');
+
+function changeColor(event){
+  setTimeout(()=>{
+    changeBtn.style.backgroundColor = 'red'
+  },2000)
+}
+changeBtn.addEventListener('click',changeColor)
+//-------------------------------------------------------
+
+//Після натискання на кнопку змінювати її колір кожні 2 секунди
+
+const paintBtn = document.querySelector('.paintBtn')
+
+function paintingButton (){
+  setInterval(()=>{
+    paintBtn.style.backgroundColor = 'green'
+  },2000);
+}
+paintBtn.addEventListener('click',paintingButton);
