3030921b9e311433014e505af7d4aae8df07ae6e
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-02 16:35:49 +0200

    Add Promise, fetch,then,catch

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
 "use strict";
 
-//JSON
+//Promise - це об'єкт, який може знаходитись у різних станах:
 
-const obj ={
-  number: 1,
-  string:'Nataliia',
-  array: [1,2,false],
-  boolean: true,
-  null: null,
-  object:{prop:10},
-  getNumber(){     // методи не передаються
-    return this.number;
-  },
-  undefined:undefined, //не передається
-}
-console.log(obj);
+/*promise state (painting,fullfiled, rejected) //якщо promise fullfiled(виконано)
+        painting, - виконується
+        fullfiled, - виконано
+        rejected  - відхилино
+result
 
-//Передача даних з об'єкту на сервер
-//obj => JSON - серіалізаія
-const objJson = JSON.stringify(obj);
-console.log(objJson);
 
-//Зворотня передача даних з серверу на JSON
-//JSON => obj   - десеріалізаія
-const parseObj = JSON.parse(objJson);
-console.log(parseObj);
+*/
+
+//setInterval(cb,1000)
+//button.addEventListener('click',()=>{});
+
+//fetch - це функція для завантаження даних
+const weatherUrl =
+  "https://apil.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m";
+
+  fetch(weatherUrl)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(err => console.log('error:',err))
+
+  //Альтернативний варіант
+  // fetch(weatherUrl)
+  // .then(response => response.json())
+  // .then(
+  //   data => console.log(data),
+  //   err => console.log('error:',err)
+  // );
+
+
+
+console.log("next line");
+console.log(weatherUrl);
+
+//console.log(response)
+//-------------------------------------
+//promise
+
+// fetch(weatherUrl)
+// .then(response => response.json())
+// .then(data => console.log(data))
+// .then(data => console.log(data))
+// .then(data => console.log(data))
+// .then(data => console.log(data))
+// .then(data => console.log(data))
+// .then(data => console.log(data))
+// .then(data => console.log(data))
+// .then(data => console.log(data))
+// .catch(err => console.log('error:',err))
+
+//--------------------------------------------
+//calback hell
+
+// setInterval(()=>{
+//   setInterval(()=>{
+//     setInterval(()=>{
+//       setInterval(()=>{
+//         setInterval(()=>{
+//           setInterval(()=>{},1000);
+//         },1000);
+//       },1000);
+//     },1000);
+//   },1000);
+// },1000);
